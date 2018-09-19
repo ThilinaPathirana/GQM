fe4da6710ed86bdf81de26448bff21e992c2f181
Thilina Pathirana <p.thilina@mubasher.net>
2018-09-19 11:32:21 +0530

    child module uncommented in approutes

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import {MainComponent} from './app-layouts/main/main.component';
 import {WidgetTypes} from './app-constants/enums/widget-types.enum';
 import {AppLayoutsModule} from './app-layouts/app-layouts.module';
+import {HomePageWidgetsModule} from './app-widgets/home-page-widgets/home-page-widgets.module';
 
 export const routes: Routes = [
   {
     path: 'home', component: MainComponent,
-    // children: [
-    //   {
-    //     path: '',
-    //     loadChildren: './app-widgets/home-page-widgets/home-page-widgets.module#HomePageWidgetsModule',
-    //   },
-    // ],
+    children: [
+      {
+        path: '',
+        loadChildren: './app-widgets/home-page-widgets/home-page-widgets.module#HomePageWidgetsModule',
+      },
+    ],
   },
 ];
