20f7be00f4a7f5d34ee208c492a84b58fa3a47a6
ShanakaBandara <shanaka.15@cse.mrt.ac.lk>
2019-05-27 13:55:58 +0530

    training module grid

--- src/app/app-widgets/training/training-front/training-front.component.ts
+++ src/app/app-widgets/training/training-front/training-front.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-training-front',
@@ -9,12 +9,21 @@ export class TrainingFrontComponent implements OnInit {
 
   title = 'app';
 
-  columnDefs = [
+  topOptions = {alignedGrids: [], suppressHorizontalScroll: true};
+  bottomOptions = {alignedGrids: [], suppressHorizontalScroll: true};
+
+  @ViewChild('topGrid') topGrid;
+  @ViewChild('bottomGrid') bottomGrid;
+
+  columnDefsTop = [
     {headerName: 'Ref#', field: 'ref', width: 100 },
     {headerName: 'Staff No', field: 'system', width: 100 },
     {headerName: 'Name', field: 'companyName', width: 100},
     {headerName: 'Department', field: 'address', width: 100 },
     {headerName: 'Position', field: 'cPerson', width: 100  },
+
+    // {headerName: 'Total', field: 'total', width: 100,  valueGetter: 'data.Phone + data.rPerson + data.rPosition',},
+
     {headerName: 'D1', field: 'Phone', width: 100  },
     {headerName: 'D2', field: 'rPerson', width: 100  },
     {headerName: 'D3', field: 'rPerson', width: 100  },
@@ -23,15 +32,72 @@ export class TrainingFrontComponent implements OnInit {
     {headerName: 'Planned', field: 'rPosition', width: 100  },
     {headerName: 'Completed from planned', field: 'product', width: 100  },
     {headerName: 'Achieve', field: 'service', width: 100 },
+
+    {
+      headerName: 'Total',
+      field: 'total',
+      valueGetter: 'data.gold + data.silver + data.bronze',
+      width: 200
+    },
+    {headerName: 'Gold', field: 'gold', width: 100},
+    {headerName: 'Silver', field: 'silver', width: 100},
+    {headerName: 'Bronze', field: 'bronze', width: 100}
   ];
 
-  rowData = [
+  rowDataTop = [
     { ref: 'Toyota', system: 'Celica', companyName: 'jjjjj', address: 'wdqwd', cPerson: 'dscdc', product: 'vfskvlfn' },
     { ref: 'Toyota', system: 'Celica', companyName: 'jjjjj', address: 'wdqwd', cPerson: 'dscdc', product: 'vfskvlfn' },
     { ref: 'Toyota', system: 'Celica', companyName: 'jjjjj', address: 'wdqwd', cPerson: 'dscdc', product: 'vfskvlfn' },
   ];
+
+  bottomData = [
+    {
+      ref: '#Training plan',
+      System: '15 - 61',
+      companyName: 'Ireland',
+      address: '2020',
+      gold: 55,
+      silver: 65,
+      bronze: 12
+    },
+    {
+      ref: '#Not achieved',
+      System: '15 - 61',
+      companyName: 'Ireland',
+      address: '2020',
+      gold: 55,
+      silver: 65,
+      bronze: 12
+    },
+    {
+      ref: '#Achieved',
+      System: '15 - 61',
+      companyName: 'Ireland',
+      address: '2020',
+      gold: 55,
+      silver: 65,
+      bronze: 12
+    },
+    {
+      ref: '% of Achieved',
+      System: '15 - 61',
+      companyName: 'Ireland',
+      address: '2020',
+      gold: 55,
+      silver: 65,
+      bronze: 12
+    }
+
+  ];
+
+
+
   constructor() { }
   ngOnInit() {
+
+    this.topOptions.alignedGrids.push(this.bottomOptions);
+    this.bottomOptions.alignedGrids.push(this.topOptions);
+
   }
 
 }
