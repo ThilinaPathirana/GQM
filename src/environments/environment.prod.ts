80c546e45b300b0bb85fac1f1edefc928a498474
Thilina Pathirana <p.thilina@theglobalmarketaccess.com>
2020-03-18 20:01:43 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   appLogLevel: 4,
   serverLogLevel: 0,
   enableRouteTracing: false,
-  doNotRedirectToLoginOnReload: false,    // if this is true reloading rubix will not refirect to login page for reauthentication
+  doNotRedirectToLoginOnReload: false,
   appVersion: 1.1815,
   consoleWarning: [{
-    text: 'Welcome to Rubix developer console...!',
+    text: 'Welcome to GTS developer console...!',
     style: 'color: blue; font-family: "Times New Roman", Times, serif; font-weight:bold; font-size: 2em',
   }],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   consoleWarning: [{
-    text: 'Welcome to Rubix developer console...!',
+    text: 'Welcome to GTS developer console...!',
     style: 'color: blue; font-family: "Times New Roman", Times, serif; font-weight:bold; font-size: 2em',
   }],
 
@@ -13,8 +13,8 @@ export const environment = {
   appLogLevel: 4,
   serverLogLevel: 0,
   enableRouteTracing: false,
-  doNotRedirectToLoginOnReload: false,    // if this is true reloading rubix will not refirect to login page for reauthentication
-  appVersion: 1.1815,    // require('../app-config.json').buildParams.version,
+  doNotRedirectToLoginOnReload: false,
+  appVersion: 1.1815,
 };
 
 /*
